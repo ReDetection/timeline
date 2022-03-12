554103d06679ce3df7e7d5e7c7fa069aea9b978c
Serg Buglakov <sbuglakov@xiag.ch>
2022-03-12 14:45:37 +0700

    Initial Commit

---
+++ Shared/timelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  timelineApp.swift
+//  Shared
+//
+//  Created by Serg Buglakov on 12.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct timelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
