57e1a898d93cd0b790510fd664e5c9fa1ff6099c
Serg Buglakov <sbuglakov@xiag.ch>
2022-04-03 00:00:03 +0700

    combine two state variables into one, ensure safety

--- TimelineCore/Logic/PureCounter.swift
+++ TimelineCore/Logic/PureCounter.swift
@@ -2,16 +2,15 @@ import Foundation
 
 public class Counter<Key: Hashable> {
     public var statistics: [Key: TimeInterval] {
-        if active, let lastKey = lastKey {
+        if case .active(let lastKey) = active {
             start(key: lastKey)
         }
         return stats
     }
-    private(set) var active = false
+    private(set) var active: Status = .off
     private let queue = DispatchQueue(label: "Counter")
     private var stats: [Key: TimeInterval] = [:]
     private var lastStateChange: Date
-    private var lastKey: Key?
     let time: () -> Date
     
     public init(timeDependency: @escaping () -> Date = { return Date() }) {
@@ -26,31 +25,33 @@ public class Counter<Key: Hashable> {
     }
     
     private func startUnsafe(key newValue: Key) {
-        if active, let lastKey = lastKey {
+        if case .active(let lastKey) = active {
             let timeInPreviousState = time().timeIntervalSince(lastStateChange)
             let previouslySavedInterval = stats[lastKey] ?? 0
             stats[lastKey] = previouslySavedInterval + timeInPreviousState
         }
         lastStateChange = time()
-        lastKey = newValue
-        active = true
+        active = .active(key: newValue)
     }
     
     public func pause() {
         queue.sync {
-            if active, let lastKey = lastKey {
+            if case .active(let lastKey) = active {
                 startUnsafe(key: lastKey)
             }
-            active = false
+            active = .off
         }
     }
     
     public func clearAndPause() {
         queue.sync {
             self.stats = [:]
-            self.lastKey = nil
-            self.active = false
+            self.active = .off
         }
     }
     
+    enum Status {
+        case active(key: Key)
+        case off
+    }
 }
