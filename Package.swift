612481ebfb4ea11bec7a161d5a96dcdc3dd96861
Serg Buglakov <redetection@gmail.com>
2022-05-01 21:29:55 +0700

    more readable way to make platforms conditional

--- Package.swift
+++ Package.swift
@@ -3,36 +3,13 @@
 
 import PackageDescription
 
-func platformDependentTargets() -> [Target] {
-#if os(macOS)
-    return [
-        .executableTarget(
-            name: "Timeline",
-            dependencies: ["TimelineCore", "SQLiteStorage"],
-            path: "macOS/",
-            resources: [
-                .copy("macOS/macOS.entitlements"),
-                .copy("macOS/timeline--macOS--Info.plist"),
-            ]),
-    ]
-#else
-    return [
-        .executableTarget(
-            name: "Timeline",
-            dependencies: ["TimelineCore", "testing_utils"],
-            path: "linux/")
-    ]
-#endif
-}
 
 let package = Package(
     name: "timeline",
     platforms: [
-        .macOS(.v12),
+        .macOS(.v12)
     ],
-    products: [
-        .executable(name: "Timeline", targets: ["Timeline"])
-    ],
+    products: [],
     dependencies: [
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", "0.13.3"..<"0.14.0"),
     ],
@@ -56,5 +33,28 @@ let package = Package(
             name: "TimelineCoreTests",
             dependencies: ["TimelineCore", "testing_utils"],
             path: "TimelineCoreTests/"),
-    ] + platformDependentTargets()
+    ]
 )
+
+#if os(macOS)
+// only brave use this, as SPM does not support macOS apps. Use xcodeproj instead
+package.products += [.executable(name: "Timeline", targets: ["TimelineCocoa"])]
+package.targets += [.executableTarget(
+    name: "TimelineCocoa",
+    dependencies: ["TimelineCore", "SQLiteStorage"],
+    path: "macOS/",
+    resources: [
+        .copy("macOS/macOS.entitlements"),
+        .copy("macOS/timeline--macOS--Info.plist"),
+    ]),
+]
+
+#elseif os(Linux)
+package.products.append(.executable(name: "Timeline", targets: ["Timeline"]))
+package.targets.append(.executableTarget(
+    name: "Timeline",
+    dependencies: ["TimelineCore", "testing_utils"],
+    path: "linux/")
+)
+#endif
+
